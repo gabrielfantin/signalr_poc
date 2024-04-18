282e6677591c06326044a9169393ff6c617a03f0
Gabriel Fantin <gabriel@autodoc.com.br>
2024-04-18 17:19:23 -0300

    feat: Add RabbitMQ listener as a long running BackgroundService

--- wss_service/Program.cs
+++ wss_service/Program.cs
@@ -1,7 +1,15 @@
+using RabbitMQ.Client;
+using wss.Services;
 using wss.SignalHubs;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
+
+builder.Services.AddHostedService<RabbitMQListener>();
+builder.Services.AddSingleton<IConnectionFactory>(sp => new ConnectionFactory()
+{
+    HostName = builder.Configuration["RabbitMq:Host"],
+});
 
 var app = builder.Build();
 
---
+++ wss_service/Services/RabbitMQListener.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace wss.Services;
+
+public class RabbitMQListener(IConnectionFactory connectionFactory) : BackgroundService
+{
+    private readonly IConnectionFactory _connectionFactory = connectionFactory;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        Console.WriteLine("Listening the broker for messages");
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (sender, args) =>
+        {
+            var body = args.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine($"Received message from broker: {message}");
+        };
+
+        channel.BasicConsume("default", true, consumer);
+
+        // Keep the task long running if it's not cancelled from outside
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(5000, stoppingToken);
+        }
+    }
+}
